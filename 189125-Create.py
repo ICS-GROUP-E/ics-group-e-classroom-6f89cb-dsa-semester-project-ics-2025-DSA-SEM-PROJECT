48ce76582c1fdb5f77a7dd4346ed7fb77805fe5b
jerim <jerimielid7@gmail.com>
2025-06-21 06:43:10 +0300

    Cate's files

---
+++ 189125-Create.py
@@ -0,0 +1,30 @@
+import mysql.connector
+new_medicines=[]
+def create_medicine(name,quantity,price,expiry):
+  new_medicines.append([name,quantity,price,expiry])
+  try:
+     conn=mysql.connector.connect (
+        host="localhost",
+        user="root",
+        passwd="",
+        database="medicines"
+     )
+     cursor=conn.cursor()
+     sql = "INSERT INTO meddata  (Name,Quantity,Price,Expiry) VALUES (%s,%s,%s,%s)"
+     values = (name,quantity,price,expiry)
+     cursor.execute(sql,values)
+     conn.commit()
+#xx
+     print(f"Successfully added medicine: {name}")
+  except mysql.connector.Error as err:
+     print(f"Something went wrong: {err}")
+  finally:
+     if conn.is_connected():
+        conn.close()
+
+name = input("Please enter your medicine name: ")
+quantity = int(input("Please enter your quantity: "))
+price = float(input("Please enter your price: "))
+expiry = float(input("Please enter your expiry: "))
+
+create_medicine(name, quantity, price, expiry)
