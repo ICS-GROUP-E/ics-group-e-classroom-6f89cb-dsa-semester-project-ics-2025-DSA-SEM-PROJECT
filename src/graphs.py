2c898fd7407d845790ebb86e881fbb4c6611b923
ishmael <ishmaelayallo@gmail.com>
2025-07-03 11:03:48 +0300

    Graphs Final

--- src/graphs.py
+++ src/graphs.py
@@ -1,6 +1,49 @@
 class Graphs:
-    pass
+    def __init__(self):
+        self.graph = {}
 
-class Distance:
-    pass
+    def add_book_node(self, title):
+        if title not in self.graph:
+            self.graph[title] = []
+
+    def add_edge(self, book1, book2):
+        # we'll use this to connect similar books
+        if book1 in self.graph and book2 in self.graph:
+            if book2 not in self.graph[book1]:
+                self.graph[book1].append(book2)
+            if book1 not in self.graph[book2]:
+                self.graph[book2].append(book1)
+
+
+    def get_recommendations(self, title):
+        # here we have BFS traversal to recommend the books
+        if title not in self.graph:
+            return []
+
+        visited = set()
+        recommendations = ()
+        queue = [title]
+
+        while queue:
+            current_book = queue.pop(0)
+            if current_book not in visited:
+                visited.add(current_book)
+                recommendations.append(current_book)
+                for neighbor in self.graph[current_book]:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+
+        recommendations.remove(title)
+        return recommendations[:5] #Return 5 recommendations
+
+
+if __name__ == "__main__":
+    bg = Graphs()
+    bg.add_book_node("Book A")
+    bg.add_book_node("Book B")
+    bg.add_book_node("Book C")
+    bg.add_edge("Book A","Book B")
+    bg.add_edge("Book B", "Book C")
+    print(bg.get_recommendations("Book A"))
+
 
