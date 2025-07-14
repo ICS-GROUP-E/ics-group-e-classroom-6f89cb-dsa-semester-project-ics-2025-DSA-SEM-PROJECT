0458863f822985398bea43bbb51fcba2da52893b
Jeremy <jerimielid7@gmail.com>
2025-07-14 21:01:00 +0300

    Update FullProject.py

--- FullProject.py
+++ FullProject.py
@@ -79,4 +79,36 @@ def undo_delete():
 
 
 
+# Reading database as a dictionary
+import mysql.connector
+
+conn2 = mysql.connector.connect(host="localhost", user="root", passwd="", database="medicines")
+
+if conn2.is_connected():
+    print('Connected to MySQL')
+else:
+    print('Failed to connect to MySQL')
+
+path = conn2.cursor()
+path.execute("SELECT Name, Quantity, Price, Expiry FROM meddata")
+ans = path.fetchall()  # Brings the rows back as tuples
+
+# DB Operations
+Medicine_Types = {}
+
+for row in ans:
+    Name, Quantity, Price, Expiry = row
+    Medicine_Types[Name] = {"Quantity": Quantity, "Price": Price, "Expiry Date": Expiry}
+
+req = input("What drug are you looking for?: ")
+print(Medicine_Types[req])
+
+
+
+
+
+
+
+
+
 
